600518d416ffd97cbe7228c695c5be781af931c4
Miguel <migvargasp@gmail.com>
2020-12-15 12:12:41 -0500

    fots

--- tar_control/settings.py
+++ tar_control/settings.py
@@ -139,5 +139,5 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/home/control/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/home/master/multimedia/media')
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
